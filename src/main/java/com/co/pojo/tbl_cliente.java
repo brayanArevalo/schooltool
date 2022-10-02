8763a1ee8ee9b320d5b1f3eef06b310a09b068a1
brayanArevalo <114872394+brayanArevalo@users.noreply.github.com>
2022-10-02 11:53:03 -0500

    Add files via upload

---
+++ src/main/java/com/co/pojo/tbl_cliente.java
@@ -0,0 +1,17 @@
+package com.co.pojo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tbl_cliente")
+public class tbl_cliente implements Serializable {
+    @Id
+    private Integer clien_id;
+    private String clien_user;
+    private String clien_password;
+}
---
+++ src/test/java/com/co/SchoolTool/SchoolToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.SchoolTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
