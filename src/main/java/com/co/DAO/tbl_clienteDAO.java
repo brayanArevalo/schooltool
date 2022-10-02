4a07a344133b98cf60662d573bfe5ecb419d43ca
brayanArevalo <114872394+brayanArevalo@users.noreply.github.com>
2022-10-02 12:23:19 -0500

    Delete src directory

--- src/main/java/com/co/DAO/tbl_clienteDAO.java
+++
@@ -1,8 +0,0 @@
-package com.co.DAO;
-
-import com.co.pojo.tbl_cliente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface tbl_clienteDAO extends CrudRepository<tbl_cliente,Integer>{
-    
-}
